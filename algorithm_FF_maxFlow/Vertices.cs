487e9a32446d8d7929916860ff8d428485e0bfe7
viktorlv30 <viktor.lv.30@gmail.com>
2016-12-19 21:31:38 +0300

    add FordFulkerson class with full algorithm

---
+++ algorithm_FF_maxFlow/Vertices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_FF_maxFlow
+{
+    public enum Vertices
+    {
+        WHITE = 0,
+        GREY = 1,
+        BLACK = 2
+    }
+}
